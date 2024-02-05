f6387d6b1b54323207c4ee9337b51362a858c02a
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-02-06 01:11:20 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Header/Header.jsx 	modified:   src/components/Header/Header.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -13,6 +15,27 @@ const Header = () => {
           Until the end of the sale: <p>45</p> days <p>06</p> hours <p>55</p>
           minutes <p>51</p>Secs.
         </h5>
+      </div>
+      <div className="header">
+        <div className="header-row-first">
+          <div className="links">
+            <Link>About Us</Link>
+            <Link>My Account</Link>
+            <Link>Wishlist</Link>
+            <p>
+              We deliver to you every day from <strong>7:00 to 23:00</strong>
+            </p>
+          </div>
+          <div className="header-details">
+            <p>
+              English <FaAngleDown />{" "}
+            </p>
+            <p>
+              USD <FaAngleDown />{" "}
+            </p>
+            <p>Order Tracking </p>
+          </div>
+        </div>
       </div>
     </>
   );
