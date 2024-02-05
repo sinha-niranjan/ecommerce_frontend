cd220243d7c25dcd77b92f5394fdfddee495cf85
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-02-06 00:36:24 +0530

    Changes to be committed: 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	new file:   public/logo.png 	new file:   src/assets/images/logo.png 	modified:   src/components/Header/Header.jsx 	new file:   src/components/Header/Header.scss 	modified:   src/index.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="discount">
+        <h4>
+          FREE delivery & 40% Discount for next 3 orders! Place your 1st order
+          in.
+        </h4>
+        <h5>
+          Until the end of the sale: <p>45</p> days <p>06</p> hours <p>55</p>
+          minutes <p>51</p>Secs.
+        </h5>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
