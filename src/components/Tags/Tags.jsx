49959aaab330b5b3aeba91702735f283dd7b7fb0
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-02-09 00:22:43 +0530

    Changes to be committed: 	renamed:    src/components/Tags/Tags.js -> src/components/Tags/Tags.jsx

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Org from "../../assets/images/logo/organic.png";
 import Cold from "../../assets/images/logo/cold.png";
 import Marked from "../../assets/images/logo/markedStar.png";
-import Unmarkded from "../../assets/images/logo/UnmarkedStar.png";
+import Unmarkded from "../../assets/images/logo/Unmarkedstar.png";
 
 const Organic = () => {
   return <img src={Org} alt="" style={{ width: "80px", height: "auto" }} />;
