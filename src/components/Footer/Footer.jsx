f79e336c3927953ce342824866e624cc787699d7
Niranjan Kumar <niranjankumaraina@gmail.com>
2024-02-11 00:49:23 +0530

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-right">
+          <h3>Join our newsletter for ₹ 100 offs</h3>
+          <p>
+            Register now to get latest updates on promotions & coupons.Don’t
+            worry, we not spam!
+          </p>
+        </div>
+        <div className="footer-top-left">
+          <div className="footer-top-left-input-box">
+            <input type="text" placeholder="Enter your email address" />
+            <span>SEND</span>
+          </div>
+          <span>
+            {" "}
+            By subscribing you agree to our{" "}
+            <p>Terms & Coditions and Privacy & Cookies Policy</p>{" "}
+          </span>
+        </div>
+      </div>
 
-export default Footer
+      <div className="footer-bottom">
+        <p>Copyright 2024 © Grogin WooCommerce WordPress Theme. All right reserved. Powered by KLBTheme.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
